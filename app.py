345ca146da10694c0807aade1fb57ae19da8a7c3
Daniele Mingolla <danielemingolla98ta@gmail.com>
2023-08-25 20:27:03 +0200

    add html template

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ from dotenv import load_dotenv
 import pandas as pd
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
 # TODO: this db for text embeddings is not permanent
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from html_templates import css, bot_template, user_template
 
 def get_json_text(json_docs):
     df_list = []
     for j_file in json_docs:
         df = pd.read_json(j_file, lines=True)
+        st.write(df[0:1000])
         # TODO: could be usefull to add as col the value in the col 'style'
         df = df[['overall', 'reviewTime', 'reviewText', 'summary']]
         df_list.append(df)
     final_df = pd.concat(df_list)
-    print(final_df.head(10))
     return final_df.to_string(header=False)
         
 def get_text_chunks(text):
@@ -32,12 +36,42 @@ def get_vector_store(text_chunks):
     vector_store = FAISS.from_texts(texts= text_chunks, embedding=embeddings)
     return vector_store
 
+def get_conversation_chain(vector_store):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    # TODO: understand it better
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever=vector_store.as_retriever(),
+        memory = memory
+    )
+    return conversation_chain
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="ChatBot for Amazon Reviews", page_icon='🤖')
+    # TODO: dont reccomended to use this option set to true
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("ChatBot for Amazon Reviews 🤖")
-    st.text_input("Ask a question about your documents:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_user_input(user_question)
+
     with st.sidebar:
         st.subheader("Your documents")
         json_docs = st.file_uploader("Upload your JSON here and click on 'Process'", accept_multiple_files=True, type=['json'])
@@ -52,7 +86,6 @@ def main():
                     # create vector store
                     vector_store = get_vector_store(text_chunks)
                     # create conversation chain
-                    #conversation = get_conversation_chain(vector_store)
-
+                    st.session_state.conversation = get_conversation_chain(vector_store)
 if __name__ == '__main__':
     main()
